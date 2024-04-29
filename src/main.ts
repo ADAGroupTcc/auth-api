748f5ffb2d2ff6921f6d91774b563a51c84d3c42
RafalFDS2 <rafael.ferraz@picpay.com>
2024-04-28 21:12:03 -0300

    feat: initial template + health endpoint

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+  );
+  await app.listen(process.env.PORT);
+}
+bootstrap();
