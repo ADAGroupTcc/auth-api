2ca264cc93727599ac5ba09ed2aa143534d2ed05
RafalFDS2 <rafael.ferraz@picpay.com>
2024-05-01 20:27:09 -0300

    teste

--- install_and_build.sh
+++ install_and_build.sh
@@ -1,7 +1,19 @@
 echo "Installing dependencies..."
+# Install or update nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+node -v
+# Install the latest version of Node.js
+nvm install node
+
+# Use the latest version
+nvm use node
+
 bun install
 
 echo "Building the project..."
 bun run build
+
+
 
 echo "Installation and build completed."
