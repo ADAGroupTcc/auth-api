0dd06eaa427b35b538221960659e2d08a07dad77
RafalFDS2 <rafael.ferraz@picpay.com>
2024-05-01 21:02:50 -0300

    remove api-token validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,16 +7,16 @@ export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
   @Post(":cpf/session")
-  session(@Param('cpf') cpf: number, @Headers("Api-Token") token: string, @Body() body: SessionDto) {
+  session(@Param('cpf') cpf: number, @Body() body: SessionDto) {
     if (!body.expirationDate) {
       body.expirationDate = Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 7);
     }
-    return this.authService.session(token, body.expirationDate, cpf);
+    return this.authService.session(body.expirationDate, cpf);
   }
 
   @Post(":cpf/auth")
   @HttpCode(200)
-  auth(@Param('cpf') cpf: number, @Headers("Api-Token") token: string, @Body() body: AuthBodyDto) {
-    return this.authService.auth(cpf, token, body.token);
+  auth(@Param('cpf') cpf: number, @Body() body: AuthBodyDto) {
+    return this.authService.auth(cpf, body.token);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,48 +6,36 @@ export class AuthService {
   private readonly logger: Logger = new Logger(AuthService.name);
   constructor(private readonly jwtService: JwtService) { }
 
-  async session(apiToken: string, expirationDate: number, cpf: number) {
+  async session(expirationDate: number, cpf: number) {
     const currentUnixTime = Math.floor(Date.now() / 1000);
     if (expirationDate < currentUnixTime) {
       throw new UnauthorizedException("expiration date is in the past");
     }
-    if (apiToken == process.env.API_TOKEN) {
-      const user = await this.findUser(cpf)
-      if (user.length != 1) {
-        throw new UnauthorizedException();
-      }
-      const payload = {
-        cpf,
-        expirationDate
-      };
-      const token = await this.jwtService.signAsync(payload);
-      return { token: btoa(token) };
-    } else {
+    const user = await this.findUser(cpf)
+    if (user.length != 1) {
       throw new UnauthorizedException();
     }
+    const payload = {
+      cpf,
+      expirationDate
+    };
+    const token = await this.jwtService.signAsync(payload);
+    return { token: btoa(token) };
   }
 
-  async auth(cpf: number, apiToken: string, token: string) {
-    if (apiToken == process.env.API_TOKEN) {
-      try {
-        const user = await this.findUser(cpf)
-        if (user.length != 1) {
-          throw new UnauthorizedException();
-        }
-        const payload: any = await this.jwtService.verifyAsync(atob(token));
-        if (payload.expirationDate <= Math.floor(Date.now() / 1000)) {
-          throw new UnauthorizedException();
-        }
-        if (payload.cpf !== cpf) {
-          throw new UnauthorizedException();
-        }
-        delete payload.iat;
-      } catch (error) {
-        throw new UnauthorizedException();
-      }
-    } else {
+  async auth(cpf: number, token: string) {
+    const user = await this.findUser(cpf)
+    if (user.length != 1) {
       throw new UnauthorizedException();
     }
+    const payload: any = await this.jwtService.verifyAsync(atob(token));
+    if (payload.expirationDate <= Math.floor(Date.now() / 1000)) {
+      throw new UnauthorizedException();
+    }
+    if (payload.cpf !== cpf) {
+      throw new UnauthorizedException();
+    }
+    delete payload.iat;
   }
 
   async findUser(cpf: number): Promise<User[]> {
@@ -60,7 +48,6 @@ export class AuthService {
       return data.users
     } catch (error) {
       this.logger.error(error)
-
       throw new UnauthorizedException();
     }
   }
